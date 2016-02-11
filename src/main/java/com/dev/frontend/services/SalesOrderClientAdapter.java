5db9770b8155ed62ef2a7f37ea15baf79eeadd31
himanshutaneja <himanshu.taneja1982@gmail.com>
2016-02-11 23:15:42 +0530

    commit

--- src/main/java/com/dev/frontend/services/SalesOrderClientAdapter.java
+++ src/main/java/com/dev/frontend/services/SalesOrderClientAdapter.java
@@ -15,19 +15,20 @@ public class SalesOrderClientAdapter {
 
 	private static final String SALES_ORDER_APP = "http://localhost:8080/SalesOrderApp/";
 
-	public static <T> List<T> getList(String path,
-			GenericType<ArrayList<T>> responseType) {
+	public static <T> List<T> getList(String path, Class<T> clas) {
 		ResteasyClient client = new ResteasyClientBuilder().build();
 		Response response = client.target(SALES_ORDER_APP + path).request()
 				.get();
-		return response.readEntity(responseType);
+		return response.readEntity(new GenericType<ArrayList<T>>() {
+		});
 	}
 
-	public static <T> T get(String path, GenericType<T> responseType) {
+	public static <T> T get(String path, Class<T> clas) {
 		ResteasyClient client = new ResteasyClientBuilder().build();
 		Response response = client.target(SALES_ORDER_APP + path).request()
 				.get();
-		return response.readEntity(responseType);
+		return response.readEntity(new GenericType<T>() {
+		});
 	}
 
 	public static <T> void save(String path, T data) {
@@ -43,13 +44,15 @@ public class SalesOrderClientAdapter {
 				.delete();
 		System.out.println(response.getStatus());
 	}
-
+	//
 	// public static void main(String[] args) {
 	// String path = "customers";
 	// List<Customer> customers = getList(path,
 	// new GenericType<ArrayList<Customer>>() {
 	// });
 	// System.out.println(customers.get(0).getAddress());
+	//
+	// List<Customer> list = getList(path, Customer.class);
 	//
 	// Customer customer = get(path + "/1", new GenericType<Customer>() {
 	// });
